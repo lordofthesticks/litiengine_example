98a1253f95cd627f662b6a5a40ae712bc26f3aa0
Terrell Page <tpage46@gmail.com>
2019-06-11 11:25:28 -0600

    initial commit

---
+++ src/litiengineexample/HelloLitiGame.java
@@ -0,0 +1,8 @@
+package litiengineexample;
+import de.gurkenlabs.litiengine.Game;
+public class HelloLitiGame {
+  public static void main(String[] args) {
+    Game.init();
+    Game.start();
+  }
+}
