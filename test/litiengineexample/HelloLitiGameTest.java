2d7ddc6f03cec531f34f8010bf7b7b44324cd7f3
Terrell Page <tpage46@gmail.com>
2019-06-11 11:44:56 -0600

    added Junit

---
+++ test/litiengineexample/HelloLitiGameTest.java
@@ -0,0 +1,12 @@
+package litiengineexample;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloLitiGameTest {
+
+  @Test
+  void main() {
+  }
+}
